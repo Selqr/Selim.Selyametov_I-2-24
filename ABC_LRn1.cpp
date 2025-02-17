3377dda411fb9944f5a87187144d39095d84d4d7
Selim <selimsus995@yandex.ru>
2025-02-17 21:40:58 +0300

    first commit

---
+++ ABC_LRn1.cpp
@@ -0,0 +1,22 @@
+﻿// ABCn1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_CTYPE, "RUS");
+    cout << "Введите число в двоичной системе счисления: ";
+    char binaryNum[100];
+    cin >> binaryNum;
+    int count = 0;
+    for (int i = 0; i < 100; i++)
+    {
+        if (binaryNum[i] != '\0') count++;
+        else break;
+    }
+    if (binaryNum[count - 1] == '0') cout << "число четное";
+    else cout << "число нечетное";
+}
+
